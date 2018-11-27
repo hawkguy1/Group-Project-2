2a1130d484aafe477e09dca5ec01166e8e7b5a57
Andy <andy.harringa@gmail.com>
2018-11-26 18:15:09 -0700

    added more details

--- app/public/js/reservation.js
+++ app/public/js/reservation.js
@@ -1,51 +1,51 @@
 // ----- DISPLAYS ALL RESERVATIONS ----- //
 $.get("/api/all", function (data) {
     if (data.length !== 0) {
-      for (var i = 0; i < data.length; i++) {
-        var row = $("<div>");
-        row.addClass("reservation");
-        row.append("<p>RESERVATION NAME: " + data[i].resName + "</p>");
-        row.append("<p>GUESTS: " + data[i].numGuests + "</p>");
-        row.append("<p>RESERVATION TIME:" + data[i].resTime + "</p>");
-        row.append("<p>GUEST MOBILE: " + data[i].resPhone + "</p>")
-        row.append("<p>RESERVATION MADE: " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
-        $("#reservation-area").prepend(row);
-      }
+        for (var i = 0; i < data.length; i++) {
+            var row = $("<div>");
+            row.addClass("reservation");
+            row.append("<p>RESTAURANT: " + data[i].resRestaurant + "</p>");
+            row.append("<p>MOBILE NUMBER: " + data[i].resPhone + "</p>");
+            row.append("<p>APPETIZER:" + data[i].resAppetizer + "</p>");
+            row.append("<p>TIME: " + data[i].resTime + "</p>")
+            row.append("<p>GUESTS: " + data[i].resGuests + "</p>")
+            $("#reservation-area").prepend(row);
+        }
     }
-  });
-  
-  // ----- WHEN USER MAKES RESERVATION ----- //
-  $("#reserve-submit").on("click", function (event) {
+});
+
+// ----- WHEN USER MAKES RESERVATION ----- //
+$("#reserve-submit").on("click", function (event) {
     event.preventDefault();
-  
+
     // ----- TURNS RESERVATION INTO AN OBJECT ----- //
     var newReservation = {
-      resName: $("#resName").val().trim(),
-      numGuests: $("#numGuests").val().trim(),
-      resTime: $("#resTime").val().trim(),
-      resPhone: $("#resphone").val().trim(),
-      created_at: moment().format("YYYY-MM-DD HH:mm:ss")
+        resRestaurant: $("#resName").val().trim(),
+        resMobile: $("#resMobile").val().trim(),
+        resAppetizer: $("#resAppetizer").val().trim(),
+        resTime: $("#resTime").val().trim(),
+        resGuests: $("#resGuests").val().trim(),
     };
     console.log(newReservation);
-  
+
     // ----- AJAX POST REQUEST WITH JQUERY ----- //
     $.post("/api/new", newReservation)
-      .then(function () {
-        var row = $("<div>");
-        row.addClass("reservation");
-        row.append("<p>RESERVATION NAME: " + data[i].resName + "</p>");
-        row.append("<p>GUESTS: " + data[i].numGuests + "</p>");
-        row.append("<p>RESERVATION TIME:" + data[i].resTime + "</p>");
-        row.append("<p>GUEST MOBILE: " + data[i].resPhone + "</p>")
-        row.append("<p>RESERVATION MADE: " + moment(data[i].created_at).format("h:mma on dddd") + "</p>");
-        $("#reservation-area").prepend(row);
-  
-      });
-  
+        .then(function () {
+            var row = $("<div>");
+            row.addClass("reservation");
+            row.append("<p>RESTAURANT: " + data[i].resRestaurant + "</p>");
+            row.append("<p>MOBILE NUMBER: " + data[i].resPhone + "</p>");
+            row.append("<p>APPETIZER:" + data[i].resAppetizer + "</p>");
+            row.append("<p>TIME: " + data[i].resTime + "</p>")
+            row.append("<p>GUESTS: " + data[i].resGuests + "</p>")
+            $("#reservation-area").prepend(row);
+
+        });
+
     // ----- EMPTY INPUT BOXES WITH EACH SUBMISSION ----- //
-    $("#resName").val("");
-    $("#numGuests").val("");
-    $("#resTime").val("");
+    $("#resRestaurant").val("");
     $("#resPhone").val("");
-  });
-  
+    $("#resAppetizer").val("");
+    $("#resTime").val("");
+    $("#resGuests").val("");
+});
