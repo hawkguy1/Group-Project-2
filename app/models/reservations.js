c68a09bbdace6b61dddfa4e54f70cabc88eba0d3
Andy <andy.harringa@gmail.com>
2018-11-27 21:03:35 -0700

    Current push

--- app/models/reservations.js
+++ app/models/reservations.js
@@ -4,21 +4,45 @@ var sequelize = require("../config/connection.js");
 // ----- CREATES RESERVATION MODEL TO MATCH DB ----- //
 
 module.exports = function (sequelize, resRestaurant, resMobile, resAppetizer, resTime, resGuests) {
-    var Reservations = sequelize.define("guestInfo", {
+    var guestInfo = sequelize.define("guestInfo", {
         id: {
             type: Sequelize.INTEGER,
             autoIncrement: true,
             primaryKey: true
         },
-        resRestaurant: Sequelize.INTEGER,
-        resMobile: Sequelize.STRING,
-        resAppetizer: Sequelize.STRING,
-        resTime: Sequelize.STRING,
-        resGuests: Sequelize.INTEGER,
-    }, {freezeTableName: true});
+        resRestaurant: {
+            type: Sequelize.STRING
+        },
+        resMobile: {
+            type: Sequelize.STRING
+        },
+        resAppetizer: {
+            type: Sequelize.STRING
+        },
+        resTime: {
+            type: Sequelize.STRING
+        },
+        resGuests: {
+            type: Sequelize.INTEGER
+        },
+    },
+        { freezeTableName: true });
 
-    Reservations.associate = function (models) {
-        onDelete: "cascade"
-    };
-    return Reservations;
+    // guestInfo.associate = function (models) {
+    // //     onDelete: "cascade"
+    // };
+    // guestInfo.sync({force: true}).then(function() {
+    //     return guestInfo.create({
+    //         resRestaurant: 'Fiesta Mexicana',
+    //         resMobile: 13194040210,
+    //         resAppetizer: 'Chips',
+    //         resTime: '6:00',
+    //         resGuests: 
+
+
+    //     })
+    // })
+    // guestInfo.sync ();
+    // module.exports = guestInfo;
+    return guestInfo;
 };
--- app/public/js/reservation.js
+++ app/public/js/reservation.js
@@ -3,52 +3,48 @@
 
 //MEXICAN
 $('#mexican').on('click', function () {
+
     var mexicanSelect = "<h4 style='font-weight:bold;'>Fiesta Mexicana</h4>"
-    var query = "<h5>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
-    var radioButtonMex = "<div class='form-check'><input class='form-check-input' type='radio' name='Chips' id='mexApp' value='Chips' checked><label class='form-check-label' for='exampleRadios1'>Tortilla Chips w/ Salsa</label></div>"
-    var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='chips' id='noMexApp' value='no chips'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
+    var query = "<h5 style='color: coral;'>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
+    var radioButtonMex = "<form id='appetizerMex' action='/'' method='POST'><input type='radio' name='appetizerMex' value='Chips and Salsa' id='chips' /><label for='chips' class='app' id='yes'>Tortilla Chips and Salsa $5.99</label><br><input type='radio' name='appetizerMex' value='No appetizer.' id='no' /><label for='no' class='app' id='no'>No thank you!</label></form>"
+    // var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='chips' id='noMexApp' value='no chips'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
     $(mexicanSelect).appendTo('#mexican-appetizer');
     $(query).appendTo('#mexican-appetizer');
     $(radioButtonMex).appendTo('#mexican-appetizer');
-    $(noThankYou).appendTo('#mexican-appetizer');
-
-    // var mexicanApp = $("input[type='radio'][name='chips']:checked").val(); 
-    // console.log(mexicanApp);
+    
     $("input[type='radio']").click(function () {
-        var radioValue = $("input[name='Chips']:checked").val();
+        var radioValue = $("input[name='appetizerMex']:checked").val();
         if (radioValue) {
-            alert("You Selected - " + radioValue);
-            resRestaurant = "Fiesta Mexicana";
-            resAppetizer = radioValue;
-        }
+            resRestaurant = "Fiessta Mexicana";
+            resAppetizer = radioValue;   
+        }  
         console.log(resRestaurant);
         console.log(resAppetizer);
+        $("#mexican-appetizer").text("You selected " + resRestaurant + "\nwith " + resAppetizer);
     });
-
-
 
 });
 
 //ITALIAN
 $('#italian').on('click', function () {
     var italianSelect = "<h4 style='font-weight:bold;'>Ristorante Italiano</h4>"
-    var query = "<h5>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
-    var radioButtonItl = "<div class='form-check'><input class='form-check-input' type='radio' name='bread' id='exampleRadios1' value='bread' checked><label class='form-check-label' for='exampleRadios1'>Bruschetta</label></div>"
-    var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='bread' id='exampleRadios1' value='no bread'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
+    var query = "<h5 style='color: coral;'>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
+    var radioButtonItl = "<form id='appetizerItal' action='/'' method='POST'><input type='radio' name='appetizerItal' value='Bruschetta' id='bread' /><label for='bread' class='app'>Bruschetta $5.99</label><br><input type='radio' name='appetizerItal' value='No appetizer.' id='no' /><label for='no' class='app' id='no'>No thank you!</label>"//<input id='submit' type='submit' value='submit' /></form>"
+    // var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='bread' id='exampleRadios1' value='no bread'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
     $(italianSelect).appendTo('#italian-appetizer');
     $(query).appendTo('#italian-appetizer');
     $(radioButtonItl).appendTo('#italian-appetizer');
-    $(noThankYou).appendTo('#italian-appetizer');
 
     $("input[type='radio']").click(function () {
-        var radioValue = $("input[name='bread']:checked").val();
+        $("#yes").css("background-color", "coral");
+        var radioValue = $("input[name='appetizerItal']:checked").val();
         if (radioValue) {
-            alert("You Selected - " + radioValue);
             resRestaurant = "Risterante Italiano";
             resAppetizer = radioValue;
         }
         console.log("You Selected " + resRestaurant);
         console.log(resAppetizer);
+        $("#italian-appetizer").text("You selected " + resRestaurant + " \nwith " + resAppetizer);
     });
 
 });
@@ -56,23 +52,23 @@ $('#italian').on('click', function () {
 //AMERICAN
 $('#american').on('click', function () {
     var americanSelect = "<h4 style='font-weight:bold;'>Burgers 'N' Bases</h4>"
-    var query = "<h5>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
-    var radioButtonAmr = "<div class='form-check'><input class='form-check-input' type='radio' name='salad' id='exampleRadios1' value='salad' checked><label class='form-check-label' for='exampleRadios1'>Caesar Salad Spears</label></div>"
-    var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='salad' id='exampleRadios1' value='no salad'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
+    var query = "<h5 style='color: coral;'>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
+    var radioButtonAmr = "<form id='appetizerMex' action='/'' method='POST'><input type='radio' name='appetizerAmer' value='Caesar Salad Spears' id='salad' /><label for='salad' class='app'>Caesar Salad Spears $5.99</label><br><input type='radio' name='appetizerAmer' value='No appetizer.' id='no' /><label for='no' class='app' id='no'>No thank you!</label>"//<input id='submit' type='submit' value='submit' /></form>"
+    // var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='salad' id='exampleRadios1' value='no salad'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
     $(americanSelect).appendTo('#american-appetizer');
     $(query).appendTo('#american-appetizer');
     $(radioButtonAmr).appendTo('#american-appetizer');
-    $(noThankYou).appendTo('#american-appetizer');
 
     $("input[type='radio']").click(function () {
-        var radioValue = $("input[name='salad']:checked").val();
+        $("#yes").css("background-color", "coral");
+        var radioValue = $("input[name='appetizerAmer']:checked").val();
         if (radioValue) {
-            alert("You Selected - " + radioValue);
             resRestaurant = "Burgers 'N' Bases";
             resAppetizer = radioValue;
         }
         console.log(resRestaurant);
         console.log(resAppetizer);
+        $("#american-appetizer").text("You selected " + resRestaurant + " \nwith " + resAppetizer);
     });
 
 });
@@ -80,23 +76,23 @@ $('#american').on('click', function () {
 //CHINESE
 $('#chinese').on('click', function () {
     var chineseSelect = "<h4 style='font-weight:bold;'>China Happy Food</h4>"
-    var query = "<h5>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
-    var radioButtonChi = "<div class='form-check'><input class='form-check-input' type='radio' name='dumplings' id='exampleRadios1' value='dumplings' checked><label class='form-check-label' for='exampleRadios1'>Pan-Fried Vegetable Dumplings</label></div>"
-    var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='dumplings' id='exampleRadios1' value='no dumplings'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
+    var query = "<h5 style='color: coral;'>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
+    var radioButtonChi = "<form id='appetizerMex' action='/'' method='POST'><input type='radio' name='appetizerChi' value='dumplings' id='Fried Vegetable Dumplings' /><label for='dumplings' class='app'>Fried Vegetable Dumplings $5.99</label><br><input type='radio' name='appetizerChi' value='No appetizer.' id='no' /><label for='no' class='app' id='no'>No thank you!</label>"//<input id='submit' type='submit' value='submit' /></form>"
+    // var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='dumplings' id='exampleRadios1' value='no dumplings'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
     $(chineseSelect).appendTo('#chinese-appetizer');
     $(query).appendTo('#chinese-appetizer');
     $(radioButtonChi).appendTo('#chinese-appetizer');
-    $(noThankYou).appendTo('#chinese-appetizer');
 
     $("input[type='radio']").click(function () {
-        var radioValue = $("input[name='dumplings']:checked").val();
+        $("#yes").css("background-color", "coral");
+        var radioValue = $("input[name='appetizerChi']:checked").val();
         if (radioValue) {
-            alert("You Selected - " + radioValue);
             resRestaurant = "China Happy Food";
             resAppetizer = radioValue;
         }
         console.log(resRestaurant);
         console.log(resAppetizer);
+        $("#chinese-appetizer").text("You selected " + resRestaurant + " \nwith " + resAppetizer);
     });
 
 });
@@ -104,22 +100,25 @@ $('#chinese').on('click', function () {
 //BREWERY
 $('#brewery').on('click', function () {
     var brewerySelect = "<h4 style='font-weight:bold;'>Brew-U</h4>"
-    var query = "<h5>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
-    var radioButtonBrew = "<div class='form-check'><input class='form-check-input' type='radio' name='corn' id='exampleRadios1' value='Brown Ale Caramel Corn with Pretzels & Pecans' checked><label class='form-check-label' for='exampleRadios1'>Brown Ale Caramel Corn with Pretzels & Pecans</label></div>"
-    var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='corn' id='exampleRadios1' value='No Brown Ale Caramel Corn with Pretzels & Pecans'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
+    var query = "<h5 style='color: coral;'>Would you like to add an appetizer? (30% SeatYourself Discount!)</h5>"
+    var radioButtonBrew ="<form id='appetizerMex' action='/'' method='POST'><input type='radio' name='appetizerBrew' value='Brown Ale Caramel Corn with Pretzels & Pecans' id='popcorn' /><label for='popcorn' class='app'> Brown Ale Caramel Corn with Pretzels & Pecans $5.99</label><br><input type='radio' name='appetizerBrew' value='No appetizer.' id='no' /><label for='no' class='app' id='no'>No thank you!</label>"//<input id='submit' type='submit' value='submit' /></form>"
+    // var noThankYou = "<div class='form-check'><input class='form-check-input' type='radio' name='corn' id='exampleRadios1' value='No Brown Ale Caramel Corn with Pretzels & Pecans'><label class='form-check-label' for='exampleRadios1'>No, thank you.</label></div>"
     $(brewerySelect).appendTo('#brewery-appetizer');
     $(query).appendTo('#brewery-appetizer');
     $(radioButtonBrew).appendTo('#brewery-appetizer');
-    $(noThankYou).appendTo('#brewery-appetizer');
-    var radioValue = $("input[name='corn']:checked").val();
+
     $("input[type='radio']").click(function () {
+        $("#yes").css("background-color", "coral");
+        $("#no").css("color", "coral");
+        var radioValue = $("input[name='appetizerBrew']:checked").val();
         if (radioValue) {
-            alert("You Selected - " + radioValue);
+           
             resRestaurant = "Brew-U";
             resAppetizer = radioValue;
         }
         console.log(resRestaurant);
         console.log(resAppetizer);
+        $("#brewery-appetizer").text("You selected " + resRestaurant + " with " + resAppetizer);
     });
 
 });
@@ -207,7 +206,7 @@ var resGuests;
 $('#party-two').on('click', function () {
     selected = true;
     var partySizeSelect = "<h4 style='font-weight:bold;'>Two</h4>"
-    console.log(twoTop);
+    console.log("Party of 2");
     $(partySizeSelect).appendTo('#two-top');
     if (selected) {
         resGuests = "twoTop";
@@ -218,7 +217,7 @@ $('#party-two').on('click', function () {
 //FOUR
 $('#party-four').on('click', function () {
     selected = true;
-    console.log("party of 4");
+    console.log("Party of 4");
     var partySizeSelect = "<h4 style='font-weight:bold;'>Four</h4>"
     $(partySizeSelect).appendTo('#four-top');
     if (selected) {
@@ -230,7 +229,7 @@ $('#party-four').on('click', function () {
 //SIX
 $('#party-six').on('click', function () {
     selected = true;
-    console.log("party of 6");
+    console.log("Party of 6");
     var partySizeSelect = "<h4 style='font-weight:bold;'>Six</h4>"
     $(partySizeSelect).appendTo('#six-top');
     if (selected) {
@@ -269,7 +268,7 @@ $("#submit").click(function () {
 
 $('#check-avail').on('click', function () {
     // window.location.replace('../public/confirm.html'); //---------------This is not a good choice!/
-console.log("button clicked");
+
 
 });
 // ----- DISPLAYS ALL RESERVATIONS ----- //
